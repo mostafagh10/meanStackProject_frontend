813559187ff7ea0b55900bf8dc55282fb567ee0f
Abanoubmedhatseif <abanoub.medhat.seif@gmail.com>
2024-02-23 19:01:18 +0200

    first update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent ,RegisterComponent, HttpClientModule, HomeComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  isLoggedIn: boolean = false;
+  token: any = localStorage.getItem('token');
+  
+  constructor(private router: Router,private cdRef: ChangeDetectorRef){}
+
+  ngOnInit() {
+    this.checkLoggedIn();
+  }
+
+  checkLoggedIn() {
+    const token = localStorage.getItem('token');
+    this.isLoggedIn = !!token;
+  }
+
+  logOut() {
+    this.token = localStorage.removeItem('token');
+    this.checkLoggedIn();
+  }
+}
