5e2e67dd3f239b93ccec1cab7917b5d1be9a6089
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-03-02 08:03:38 +0200

    Displayed Image in book-detail page

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BooksService } from '../services/books/books.service';
 import { Book } from '../User/Interfaces/Book';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-book-detail',
@@ -13,17 +14,28 @@ import { Book } from '../User/Interfaces/Book';
 export class BookDetailComponent {
   
   bookDetails !: any;
-  constructor(private acRoute: ActivatedRoute, private booksService: BooksService){
+  constructor(private acRoute: ActivatedRoute, private http: HttpClient, private booksService: BooksService){
     // this.src = "127.0.0.1:3000"
   } 
-  
+
+  imageSrc: any;
+
   ngOnInit(){
     const id = this.acRoute.snapshot.params['id'];
       this.booksService
           .getOneBook(id)
           .subscribe((data) => {
-              this.bookDetails = data;          
+              this.bookDetails = data; 
+              console.log(this.bookDetails.bookInfo.photo);
+              this.test(this.bookDetails.bookInfo.photo).subscribe( (image) => {
+                this.imageSrc = URL.createObjectURL(image);
+                console.log(this.imageSrc);
+              })      
             });
     }
 
+    test(photo: any){
+        const url = `http://127.0.0.1:3000/book/book-image/${photo}`;
+        return this.http.get(url, { responseType: 'blob' });
+    }
 }
